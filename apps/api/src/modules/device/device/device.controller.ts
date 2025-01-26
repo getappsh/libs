3caf60c90810e572b6af9cb4a8e12cdd7ea9ce47
David Klein <davidk@ravtech.co.il>
2025-01-26 18:55:24 +0200

    Refactor device configuration DTOs and add LayersConfigDto for improved structure and validation

--- apps/api/src/modules/device/device/device.controller.ts
+++ apps/api/src/modules/device/device/device.controller.ts
@@ -84,7 +84,7 @@ export class DeviceController {
     description: "This service message returns an object of device configurations.",
   })
   @ApiOkResponse({
-    schema: { oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
+    schema: { title: "ConfigDto", oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
   })
   getDeviceConfig(@Query('group') group: string) {
     console.log(group)
@@ -98,15 +98,15 @@ export class DeviceController {
     summary: "Set Device Configurations",
     description: "This service message returns an object of device configurations.",
   })
-  @ApiExtraModels(AndroidConfigDto, WindowsConfigDto)
+  @ApiExtraModels(AndroidConfigDto, WindowsConfigDto,)
   @ApiBody({
-    schema: { oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
+    schema: { title: "ConfigDto", oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
   })
   @ApiOkResponse({
-    schema: { oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
+    schema: { title: "ConfigDto", oneOf: [{ $ref: getSchemaPath(AndroidConfigDto) }, { $ref: getSchemaPath(WindowsConfigDto) }] }
   })
   @UsePipes(DeviceConfigValidator)
-  setDeviceConfig(@Body() config: AndroidConfigDto | BaseConfigDto) {
+  setDeviceConfig(@Body() config: AndroidConfigDto | WindowsConfigDto | BaseConfigDto) {
     this.logger.debug('Set device config')
     return this.deviceService.setDeviceConfig(config);
   }
