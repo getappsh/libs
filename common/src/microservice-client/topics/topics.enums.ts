f5df99e5ddc4bffbc52bef32406ab335d5b9e76e
David Klein <davidk@ravtech.co.il>
2025-07-03 12:48:20 +0300

    feat: add deleteGroup functionality to GroupService and corresponding MessagePattern

--- common/src/microservice-client/topics/topics.enums.ts
+++ common/src/microservice-client/topics/topics.enums.ts
@@ -190,6 +190,7 @@ export const DevicesGroupTopics = {
     GET_GROUPS: `getapp-device.group.get-all${region}`,
     GET_GROUP_DEVICES: `getapp-device.group.get-devices${region}`,
     SET_GROUP_DEVICES: `getapp-device.group.set-devices${region}`,
+    DELETE_GROUP: `getapp-device.group.delete${region}`,
 } as const
 
 export const DevicesHierarchyTopics = {
