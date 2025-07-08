f191c00edf0da89b14f5055755959ddc92c6edff
David Klein <davidk@ravtech.co.il>
2025-07-08 16:45:21 +0300

    feat: refactor GroupController and GroupService;
    - restore CRUD operations for groups
    - enhance getOrgIds method with additional filters

--- common/src/dto/devices-group/dto/org-id.dto.ts
+++ common/src/dto/devices-group/dto/org-id.dto.ts
@@ -8,31 +8,33 @@ export class OrgIdDto {
   @IsNumber()
   @IsNotEmpty()
   orgId: number;
-
+  
   toString() {
     return JSON.stringify(this);
   }
 }
 
 export class OrgIdRefDto extends OrgIdDto {
-
+  
   @ApiProperty({ required: false, type: String, nullable: true, description: "Device id related to the org id. Set to null to remove the current device." })
   @IsOptional()
   @IsString()
   device?: string | null;
-
+  
   @ApiProperty({ required: false, type: Number, nullable: true, description: "Group parent id for the org id. Set to null to remove the current group parent." })
   @IsOptional()
   @IsNumber()
   group?: number | null;
-
+  
   toString() {
     return JSON.stringify(this);
   }
 }
 
 export class OrgIdPutDto {
-
+  
+  orgId: number;
+  
   @ApiProperty({ required: false, type: String, nullable: true, description: "Device id related to the org id. Set to null to remove the current device." })
   @IsOptional()
   @IsString()
